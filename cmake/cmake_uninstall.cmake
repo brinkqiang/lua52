f65772185762e7b76c2ad93d2aacbd0f80c2d8aa
brinkqiang <brink.qiang@gmail.com>
2020-01-09 16:47:57 +0800

    add

--- cmake/cmake_uninstall.cmake
+++ cmake/cmake_uninstall.cmake
@@ -3,6 +3,6 @@ CONFIGURE_FILE(
         "${CMAKE_CURRENT_SOURCE_DIR}/cmake/cmake_uninstall.cmake.in"
         "${CMAKE_CURRENT_BINARY_DIR}/cmake_uninstall.cmake"
         IMMEDIATE @ONLY)
-ADD_CUSTOM_TARGET(UNINSTALL
+ADD_CUSTOM_TARGET(uninstall
         COMMAND ${CMAKE_COMMAND} -P ${CMAKE_CURRENT_BINARY_DIR}/cmake_uninstall.cmake)
 ENDMACRO(ADD_UNINSTALL_TARGET)
